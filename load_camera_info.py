088c834f2bfb484e56e70b3a48559ecc4bcafae8
Andrew Wagner <andrew.wagner@robovision.eu>
2017-01-27 18:21:23 +0100

    Fixing up bugs... make module name more precise

--- load_camera_info.py
+++ load_camera_info.py
@@ -81,14 +81,80 @@ def load_intrinsics(filePath):
         p1 = d['Poly4'] * .001
         p2 = d['Poly5'] * .001
         distCoeffs = (k1, k2, p1, p2, k3)
-    elseif 'Kappa' in d:
+    elif 'Kappa' in d:
         distCoeffs = (d['Kappa'],)
     else:
-        distCoeffs = (,)
+        distCoeffs = ()
 
     return cameraMatrix, distCoeffs
 
-def load_extrinsics(filePath):
+
+def rodriguez_vector_to_SO3(a1,a2,a3, implementation='giplib'):
+    """ Converts from an axis, angle rotation representation to a 3x3 matrix representation. 
+        The angles are assumed to be in radians! """
+    assert implementation in ['giplib','scipy']
+    if implementation == 'giplib':
+        # This implementation was ported over from giplib.
+        from numpy import cos,sin,sqrt
+        angle = sqrt(a1**2 + a2**2 + a3**2)
+        if angle == 0:
+            return numpy.eye(3)
+        phi = angle
+        rx = a1 / angle
+        ry = a2 / angle
+        rz = a3 / angle
+        cosPhi = cos(phi)
+        sinPhi = sin(phi)
+        l_cosPhi = 1.0 - cosPhi
+        rxRyl_cosPhi = rx * ry * l_cosPhi
+        rxRzl_cosPhi = rx * rz * l_cosPhi
+        return numpy.array([[ cosPhi+(rx**2)*l_cosPhi, rxRyl_cosPhi-rz*sinPhi, ry*sinPhi+rxRzl_cosPhi],
+                            [rz*sinPhi+rxRyl_cosPhi, cosPhi+(ry**2)*l_cosPhi, ry*rz*l_cosPhi-rx*sinPhi],
+                            [rxRzl_cosPhi-ry*sinPhi, rx*sinPhi+ry*rz*l_cosPhi, cosPhi+(rz**2)*l_cosPhi]])
+    elif implementation == 'scipy':
+        # A more straightforward way... not sure what is faster or more accurate.
+        # Added this mainly to check the correctness of above port.
+        import scipy
+        a,b,c = a1,a2,a3
+        skew = numpy.matrix([[0,-c,b],
+                            [c,0,-a],
+                            [-b,a,0]])
+        return scipy.linalg.expm(skew)
+
+
+def load_halcon_extrinsics_rodriguez(filePath):
+    """ Load directly from HALCON's ascii format for camera extrinsics."""
+    assert filePath.is_file()
+    try:
+        lines = filePath.open().readlines()
+    except:
+        print("File "+str(filePath)+" doesn't seem to be a readable text file containing extrinsics!")
+        raise
+    lines = map(lambda line: line.strip(), lines)
+    lines = filter(lambda line: line != '', lines)
+    lines = filter(lambda line: line[0] != '#', lines)
+    lines = map(lambda line: line.strip(), lines)
+    lines = list(lines)
+
+    d = {}
+    expected_keys = set(('f','r','t'))
+    for line in lines:
+        key = line[0]
+        assert key in expected_keys, "Unrecognized character at start of line while parsing extrinsics!"
+        expected_keys.remove(key)
+        if line[0]=='f':
+            assert line.split(' ')[1]=='0', 'Representation type not handled!'
+            continue
+        value = numpy.array(tuple(map(float,list(line.split(' '))[1:])))
+        d[key] = value
+
+    anglevector = d['r']*numpy.pi/180 # Supposedly the angles in the ascii files are in degrees... 
+    R = rodriguez_vector_to_SO3(anglevector[0], anglevector[1], anglevector[2])
+    t = d['t']
+    return R,t
+
+
+def load_halcon_extrinsics_homogeneous(filePath):
     """ HALCON is able to export camera extrinsics as a homogeneous matrix
         stored in an ascii text file. This export format is the easies to deal
         with.
@@ -108,3 +174,18 @@ def load_extrinsics(filePath):
     R = H[:,0:3]
     T = H[:,3]
     return R, T
+
+def load_extrinsics(filePath):
+    # Automatically detect the file format, so that this function
+    # works with both types of ascii based HALCON extrinsics formats.
+    try:
+        lines = filePath.open().readlines()
+    except:
+        print("File "+str(filePath)+" doesn't seem to be a readable text file!")
+        raise
+    for line in lines:
+        if 'Rotation angles [deg] or Rodriguez vector:' in line:
+            print('Rodriquez type HALCON extrinsics file detected!')
+            return load_halcon_extrinsics_rodriguez(filePath)
+    print('Homogeneous type HALCON extrinsics file detected!')
+    return load_halcon_extrinsics_homogeneous(filePath)
